ae63ede0536db3f2e6ea30cc7b20edb1dd5cbf6c
MoritAa102 <Moritz.Handke@gmx.de>
2026-01-22 21:56:21 +0100

    Add Firebase configuration file
    Add Firebase configuration settings for the project.

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBF_y3Nnu9vGsHVJFHpw0YD_ctKIglpiX8",
+  authDomain: "sei-wie-friedrich.firebaseapp.com",
+  databaseURL: "https://sei-wie-friedrich-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sei-wie-friedrich",
+  storageBucket: "sei-wie-friedrich.firebasestorage.app",
+  messagingSenderId: "776136993655",
+  appId: "1:776136993655:web:ede98e18ef56ef463e0f72"
+};
